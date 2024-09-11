49d9076b694c2e49eb2545d089ed1a2b09117bdf
Sami Onk <onksami@gmail.com>
2024-09-11 09:03:43 +0200

    button added

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,67 @@
-import { useCallback, useEffect, useState } from "react"
-import { HangmanDrawing } from "./HangmanDrawing"
-import { HangmanWord } from "./HangmanWord"
-import { Keyboard } from "./Keyboard"
-import words from "./wordList.json"
+import { useCallback, useEffect, useState } from "react";
+import { HangmanDrawing } from "./HangmanDrawing";
+import { HangmanWord } from "./HangmanWord";
+import { Keyboard } from "./Keyboard";
+import words from "./wordList.json";
 
 function getWord() {
-  return words[Math.floor(Math.random() * words.length)]
+  return words[Math.floor(Math.random() * words.length)];
 }
 
 function App() {
-  const [wordToGuess, setWordToGuess] = useState(getWord)
-  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+  const [wordToGuess, setWordToGuess] = useState(getWord);
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
 
   const incorrectLetters = guessedLetters.filter(
-    letter => !wordToGuess.includes(letter)
-  )
+    (letter) => !wordToGuess.includes(letter)
+  );
 
-  const isLoser = incorrectLetters.length >= 6
+  const isLoser = incorrectLetters.length >= 6;
   const isWinner = wordToGuess
     .split("")
-    .every(letter => guessedLetters.includes(letter))
+    .every((letter) => guessedLetters.includes(letter));
 
   const addGuessedLetter = useCallback(
     (letter: string) => {
-      if (guessedLetters.includes(letter) || isLoser || isWinner) return
+      if (guessedLetters.includes(letter) || isLoser || isWinner) return;
 
-      setGuessedLetters(currentLetters => [...currentLetters, letter])
+      setGuessedLetters((currentLetters) => [...currentLetters, letter]);
     },
     [guessedLetters, isWinner, isLoser]
-  )
+  );
 
   useEffect(() => {
     const handler = (e: KeyboardEvent) => {
-      const key = e.key
-      if (!key.match(/^[a-z]$/)) return
+      const key = e.key;
+      if (!key.match(/^[a-z]$/)) return;
 
-      e.preventDefault()
-      addGuessedLetter(key)
-    }
+      e.preventDefault();
+      addGuessedLetter(key);
+    };
 
-    document.addEventListener("keypress", handler)
+    document.addEventListener("keypress", handler);
 
     return () => {
-      document.removeEventListener("keypress", handler)
-    }
-  }, [guessedLetters])
+      document.removeEventListener("keypress", handler);
+    };
+  }, [guessedLetters]);
 
   useEffect(() => {
     const handler = (e: KeyboardEvent) => {
-      const key = e.key
-      if (key !== "Enter") return
+      const key = e.key;
+      if (key !== "Enter") return;
 
-      e.preventDefault()
-      setGuessedLetters([])
-      setWordToGuess(getWord())
-    }
+      e.preventDefault();
+      setGuessedLetters([]);
+      setWordToGuess(getWord());
+    };
 
-    document.addEventListener("keypress", handler)
+    document.addEventListener("keypress", handler);
 
     return () => {
-      document.removeEventListener("keypress", handler)
-    }
-  }, [])
+      document.removeEventListener("keypress", handler);
+    };
+  }, []);
 
   return (
     <div
@@ -87,15 +87,16 @@ function App() {
       <div style={{ alignSelf: "stretch" }}>
         <Keyboard
           disabled={isWinner || isLoser}
-          activeLetters={guessedLetters.filter(letter =>
+          activeLetters={guessedLetters.filter((letter) =>
             wordToGuess.includes(letter)
           )}
           inactiveLetters={incorrectLetters}
           addGuessedLetter={addGuessedLetter}
         />
       </div>
+      <button> Refresh the page</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
